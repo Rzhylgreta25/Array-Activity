7b23bc987e858f61739c2eb5850ab1c080516aa1
Your Name <rzhyl05@gmail.com>
2024-10-15 12:54:37 +0800

    your commit message

---
+++ arrayActivity.js
@@ -0,0 +1,11 @@
+let favSinger = ["Selena", "Ariana", "Jimin"];
+console.log(favSinger[1]);
+
+const favNumbers = [4,19,24,87];
+
+const mixedArr =  ["string", ["otherarray"], 123, true];
+
+console.log[0]
+console.log[1]
+console.log[2]
+console.log[3]
